efea14321c05a90cff8378282b639999e45a6ee5
Agustin <124111465+atabisi@user.noreply.github.com>
2024-03-25 22:59:54 -0300

    Changed .js logic to fix errors after equalizing

--- app.js
+++ app.js
@@ -1,85 +1,110 @@
 let currentValue = ''
-let previousValue = '0'
-let operator = '+'
+let previousValue = ''
+let operator = null
+let screenReset = false
 
-let numbers = document.querySelectorAll(".number")
-let operators = document.querySelectorAll(".operator")
-const clear = document.querySelector("#clear")
+const numberButtons = document.querySelectorAll(".number")
+const operatorButtons = document.querySelectorAll(".operator")
+const equalButton = document.querySelector(".equal")
+const decimalButton = document.querySelector(".decimal")
+const clearButton = document.querySelector(".clear")
 const previousScreen = document.querySelector("#previous")
 const currentScreen = document.querySelector("#current")
-const equal = document.querySelector(".equal")
-const decimal = document.querySelector(".decimal")
 
-numbers.forEach((number) => number.addEventListener('click', function (e) {
-    handleNumber(e.target.textContent)
-    currentScreen.textContent = currentValue;
-}))
-
-operators.forEach((op) => op.addEventListener('click', function (e) {
-    handleOperator(e.target.textContent)
-    previousScreen.textContent = previousValue + " " + operator
-    currentScreen.textContent = currentValue
-}))
-
-equal.addEventListener('click', function () {
-    calculate()
+function clear() {
+    currentScreen.textContent = '0'
     previousScreen.textContent = ''
-    if (previousValue.length < 10) {
-        currentScreen.textContent = previousValue
-    } else {
-        currentScreen.textContent = previousValue.slice(0, 10) + "...";
-    }
-})
-
-clear.addEventListener('click', function () {
-    previousValue = '0'
+    previousValue = ''
     currentValue = ''
-    operator = '+'
-    previousScreen.textContent = ''
-    currentScreen.textContent = currentValue
-})
-
-decimal.addEventListener('click', function () {
-    addDecimal();
-})
-
-function handleNumber(num) {
-    if (currentValue.length < 10) {
-        currentValue += num;
-    }
+    operator = null
 }
 
-function handleOperator(op) {
-    calculate()
+function resetScreen() {
+    currentScreen.textContent = ''
+    screenReset = false
+}
+
+numberButtons.forEach((number) => number.addEventListener('click', function (e) {
+    if (currentScreen.textContent === '0' || screenReset)
+        resetScreen()
+    currentScreen.textContent += e.target.textContent
+}))
+
+operatorButtons.forEach((button) => button.addEventListener('click', function (e) {
+    setOperator(e.target.textContent)
+}))
+
+clearButton.addEventListener('click', clear)
+
+decimalButton.addEventListener('click', addDecimal)
+
+equalButton.addEventListener('click', evaluate)
+
+function setOperator(op) {
+    if (operator !== null) evaluate()
+    previousValue = currentScreen.textContent
     operator = op
-    currentValue = ''
+    previousScreen.textContent = `${previousValue} ${operator}`
+    screenReset = true
 }
 
-function calculate() {
-    previousValue = Number(previousValue);
-    currentValue = Number(currentValue);
-
-    if (operator === "+") {
-        previousValue += currentValue;
-    } else if (operator === "-") {
-        previousValue -= currentValue;
-    } else if (operator === "*") {
-        previousValue *= currentValue;
-    } else {
-        previousValue /= currentValue;
+function evaluate() {
+    if (operator === null || screenReset) return
+    if (operator === '/' && currentScreen.textContent === '0') {
+        alert("Impossible to divide by 0.")
+        return
     }
+    currentValue = currentScreen.textContent
+    currentScreen.textContent = roundNumber(
+        operate(operator, previousValue, currentValue)
+    )
+    previousScreen.textContent = `${previousValue} ${operator} ${currentValue}`
+    operator = null
+}
 
-    previousValue = roundNumber(previousValue);
-    previousValue = previousValue.toString();
-    currentValue = currentValue.toString();
+function add(a, b) {
+    return a + b
+}
+
+function substract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function operate(op, a, b) {
+    a = Number(a)
+    b = Number(b)
+    switch (operator) {
+        case '+':
+            return add(a, b)
+        case '-':
+            return substract(a, b)
+        case '*':
+            return multiply(a, b)
+        case '/':
+            if (b === 0) return null
+            else return divide(a, b)
+        default:
+            return null
+    }
 }
 
 function roundNumber(num) {
-    return Math.round(num * 10000) / 10000;
+    return Math.round(num * 1000) / 1000;
 }
 
 function addDecimal() {
-    if (!currentValue.includes(".")) {
-        currentValue += ".";
-    }
+    if (screenReset) resetScreen()
+    if (currentScreen.textContent === '')
+        currentScreen.textContent = '0'
+    if (currentScreen.textContent.includes('.')) return
+    currentScreen.textContent += '.'
 }
+
